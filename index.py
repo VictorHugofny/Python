e6180ef002cc3411aa88f8b84f500beaf6e60a56
Victor Hugo <53804087+VictorHugofny@users.noreply.github.com>
2019-08-06 20:31:05 -0300

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,16 @@ CALCNOTES = 2
 #Variáveis Globais
 opc        = ''
 validation = False
+notes = []
 
+def InsertNotas():
+    for x in range(0, TURNS):
+        notes.append(input('\nInforme a {}º Nota: '.format(x+1)))
+
+def Listanotas():
+    for x in range(0, TURNS):
+        print('\n{}º Nota: '.format(notes))
+        
 #Menu 
 def Menu():
     Header()
@@ -15,12 +24,12 @@ def Menu():
     print('[3] - Visualizar Situação')
     print('[*] - Sair')
 
-    opc = input('>>> ')
+    opc = int(input('>>> '))
 
-    if(opc == '1'):
-        print('')
+    if(opc == 1):
+        InsertNotas()
     elif(opc == '2'):
-        print('')
+        Listanotas()
     elif(opc == '3'):
         print('')
     else:
@@ -32,3 +41,5 @@ def Header():
     print('-=' * 4, 'Gerenciamente de Notas', '-=' * 4)
 
 Menu()
+
+
